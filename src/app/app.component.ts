182a31a43a78c3e06adf590f541ec21253db8b1f
evanlin96069 <72735402+evanlin96069@users.noreply.github.com>
2024-07-14 04:06:34 +0800

    Add website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TimelineComponent } from './timeline/timeline.component';
+
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TimelineComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'portal-tas-timeline';
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
